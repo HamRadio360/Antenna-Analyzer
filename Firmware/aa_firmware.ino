48f6786877cf7877fc3af5ae722679eeca3bb9a9
Derek Kozel <derek.kozel@gmail.com>
2017-01-11 23:51:02 -0800

    Updated license header

--- Firmware/aa_firmware.ino
+++ Firmware/aa_firmware.ino
@@ -1,18 +1,17 @@
 // This file is part of the K6BEZ Antenna Analyzer project.
 //
-// This project is free software: you can redistribute it and/or modify
+// This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// This project is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with the K6BEZ Antenna Analyzer project.
-// If not, see <http://www.gnu.org/licenses/>.
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 // include the library code:
 #include <LiquidCrystal.h>
